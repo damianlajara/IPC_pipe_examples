80c3c3c770d0ed02a72eb2d5adb0afd642b209ae
Damian Lajara <damiansito93@gmail.com>
2014-03-06 23:23:59 -0500

    added some comments

--- pipe_redirection.c
+++ pipe_redirection.c
@@ -1,4 +1,11 @@
 /*
+Frequently, you'll want to create a child process and set up one end of a pipe as its standard input or standard output. 
+Using the dup2 call, you can equate one file descriptor with another. 
+For example, to redirect a process's standard input to a file descriptor fd, use this line: dup2 (fd, STDIN_FILENO); 
+The symbolic constant STDIN_FILENO represents the file descriptor for the standard input, which has the value 0. 
+The call closes standard input and then reopens it as a duplicate of fd so that the two may be used interchangeably. 
+Equated file descriptors share the same file position and the same set of file status flags.
+ Thus, characters read from fd are not reread from standard input.
 This program uses dup2 to send the output from a pipe to the SORT command.
 After creating a pipe, the program forks.The parent process prints some strings to the pipe.
 The child process attaches the read file descriptor of the pipe to its standard input using dup2. 
@@ -35,7 +42,8 @@ int main ()
 	{
 		/* This is the parent process. */
 		FILE* stream;
-		/* Close our copy of the read end of the file descriptor. */ close (fds[0]);
+		/* Close our copy of the read end of the file descriptor. */ 
+		close (fds[0]);
 		/* Convert the write file descriptor to a FILE object, and write
 		to it. */
 		stream = fdopen (fds[1], "w");
